0623d0aee3e68c0578ecb978ac8c9e6b4be6f3e0
tsm13 <thais@live.jp>
2024-04-11 15:32:43 -0300

    edit config

--- src/js/config.js
+++ src/js/config.js
@@ -1,7 +1,6 @@
 export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '8d043094-da08-4371-9d81-dff471a30e33';
-// export const KEY = 'c1c5757d-93d5-41e4-b7d4-fa2d67bc714a';
+export const KEY = 'c1c5757d-93d5-41e4-b7d4-fa2d67bc714a';
 export const MODAL_CLOSE_SEC = 2.5;
 export const ANIMATION_WAIT = 1;
